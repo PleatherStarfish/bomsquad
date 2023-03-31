75ea2e49953044425f7919a66aacc0ea57b0c2c0
Daniil.I <daniil.istomin@onix-systems.com>
2023-03-31 22:31:37 +0300

    Update entrypoint for backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,6 +6,7 @@ python manage.py createsuperuser --noinput
 if [ $PRODUCTION = True ]
 then
   cd /__frontend
+  npm i
   npm run build
   mv build/index.html /code/templates/frontend.html
   mv build/static/css/* /code/static/css
