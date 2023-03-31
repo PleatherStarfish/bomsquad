e2b0d90f9d5aa68a226c35ade87d7f0af07dd504
Daniil.I <daniil.istomin@onix-systems.com>
2023-03-31 20:39:03 +0300

    Update backend routing

--- backend/django_project/urls.py
+++ backend/django_project/urls.py
@@ -3,24 +3,38 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 from django.contrib.auth.decorators import login_required
+
 from modules import views as ModuleView
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("allauth.urls")),
-    path("api/", include("api.urls")),
+
+frontend_redirect_urls = [
+    re_path(r"^generic/.+$", TemplateView.as_view(template_name="frontend.html"), name='frontend'),
     path(
         "module/<slug:slug>/",
-        RedirectView.as_view(url="http://127.0.0.1:3000/module/%(slug)s/"),
+        TemplateView.as_view(template_name="frontend.html"),
         name="module",
     ),
     path(
         "user/<str:username>/",
-        RedirectView.as_view(url="http://127.0.0.1:3000/user/%(username)s/"),
+        TemplateView.as_view(template_name="frontend.html"),
         name="user",
     ),
+    re_path(
+        r"^(?!media|admin|accounts|api|static|module|user).*",
+        TemplateView.as_view(template_name="frontend.html"),
+    ),
+]
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path("user/", RedirectView.as_view(pattern_name='frontend')),
+    
+    path("api/", include("api.urls")),
+
     path(
         "add-to-built/<int:module_id>/",
         login_required(ModuleView.add_module_to_built),
@@ -37,9 +51,4 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Redirect all other URLs to the React app
-urlpatterns += [
-    re_path(
-        r"^(?!media|admin|accounts|api|static|module|user).*",
-        lambda request: redirect("http://127.0.0.1:3000/"),
-    ),
-]
+urlpatterns.extend(frontend_redirect_urls)
