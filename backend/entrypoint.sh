800932120cb47d3661b6265d33b3dc4b491e4859
Daniil.I <daniil.istomin@onix-systems.com>
2023-04-08 00:03:35 +0300

    Add webpack for react dev and prod

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,11 +6,8 @@ python manage.py createsuperuser --noinput
 if [ $PRODUCTION = True ]
 then
   cd /__frontend
-  npm i
+  npm install -g
   npm run build
-  mv build/index.html /code/templates/frontend.html
-  mv build/static/css/* /code/static/css
-  mv build/static/js/* /code/static/js
   cd /code
   python manage.py collectstatic --noinput --clear
   gunicorn django_project.wsgi --bind :8000 --workers 2 --reload
