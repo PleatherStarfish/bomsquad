63200a74597e8760d9db336b65036ceb96f4b58f
Daniil.I <daniil.istomin@onix-systems.com>
2023-04-08 13:30:47 +0300

    Update entrypoint for backend prod

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,10 +5,6 @@ python manage.py createsuperuser --noinput
 
 if [ $PRODUCTION = True ]
 then
-  # cd /__frontend
-  # npm install -g
-  # npm run build
-  # cd /code
   python manage.py collectstatic --noinput --clear
   gunicorn django_project.wsgi --bind :8000 --workers 2 --reload
 else
