c5eb0dd9c1a064601ad3f55ffe368497db158cdd
Daniil.I <daniil.istomin@onix-systems.com>
2023-04-08 13:29:25 +0300

    Update prod react

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,10 +5,10 @@ python manage.py createsuperuser --noinput
 
 if [ $PRODUCTION = True ]
 then
-  cd /__frontend
-  npm install -g
-  npm run build
-  cd /code
+  # cd /__frontend
+  # npm install -g
+  # npm run build
+  # cd /code
   python manage.py collectstatic --noinput --clear
   gunicorn django_project.wsgi --bind :8000 --workers 2 --reload
 else
