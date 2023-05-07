249186a46cf3738fb1e207087ff142b503c55976
Daniel <lontano.composer@gmail.com>
2023-05-06 23:41:54 -0400

    Recent changes

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -1,18 +1,10 @@
+import { Dialog, Menu, Transition } from "@headlessui/react";
 import {
-  Bars3Icon,
-  BellIcon,
-  ChartPieIcon,
-  Cog6ToothIcon,
   FolderIcon,
   HomeIcon,
-  WrenchIcon,
-  XMarkIcon,
+  ListBulletIcon,
+  WrenchIcon
 } from "@heroicons/react/24/outline";
-import {
-  ChevronDownIcon,
-  MagnifyingGlassIcon,
-} from "@heroicons/react/20/solid";
-import { Dialog, Menu, Transition } from "@headlessui/react";
 import { Link, Outlet, useLocation } from "react-router-dom";
 import React, { Fragment, useState } from "react";
 
@@ -33,14 +25,15 @@ const navigation = [
   {
     name: "Shopping List",
     to: "shopping-list",
-    icon: ChartPieIcon,
+    icon: ListBulletIcon,
     current: false,
   },
 ];
 
 const UserPage = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
+  const [selectedTab, setSelectedTab] = useState(
+    navigation.find((tab) => tab.current).name
+  );
   const { user, userIsLoading, userIsError } = useAuthenticatedUser();
 
   if (userIsLoading)
@@ -52,158 +45,48 @@ const UserPage = () => {
   };
 
   return (
-    <div className="mt-20">
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-white">
-        <body class="h-full">
-        ```
-      */}
-      <div>
-        <Transition.Root show={sidebarOpen} as={Fragment}>
-          <Dialog
-            as="div"
-            className="relative z-30 lg:hidden"
-            onClose={setSidebarOpen}
-          >
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="fixed inset-0 bg-gray-900/80" />
-            </Transition.Child>
-
-            <div className="fixed inset-0 flex">
-              <Transition.Child
-                as={Fragment}
-                enter="transition ease-in-out duration-300 transform"
-                enterFrom="-translate-x-full"
-                enterTo="translate-x-0"
-                leave="transition ease-in-out duration-300 transform"
-                leaveFrom="translate-x-0"
-                leaveTo="-translate-x-full"
-              >
-                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
-                  <Transition.Child
-                    as={Fragment}
-                    enter="ease-in-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in-out duration-300"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
-                      <button
-                        type="button"
-                        className="-m-2.5 p-2.5"
-                        onClick={() => setSidebarOpen(false)}
+    <div className="flex gap-6 mt-[64px]">
+      <div
+        className="w-[70px] group/slideout hover:w-[200px] bg-gray-100 fixed transition-all duration-300 ease-in"
+        style={{ height: "calc(100vh - 64px)" }}
+      >
+        <div className="flex flex-col h-full px-6 pb-4 overflow-y-auto bg-gray-100 gap-y-5">
+          <nav className="flex flex-col flex-1 h-full">
+            <ul role="list" className="flex flex-col flex-1 h-full gap-y-7">
+              <li>
+                <ul role="list" className="mt-8 -mx-2 space-y-1">
+                  {navigation.map((item) => (
+                    <li key={item.name}>
+                      <Link
+                        key={item.name}
+                        to={item.to}
+                        className={cx(
+                          item.current
+                            ? "bg-gray-200 text-[#548a6a]"
+                            : "text-gray-700 hover:text-[#548a6a]",
+                          "group/item flex gap-x-3 rounded-md px-2 py-3 text-sm leading-6 font-semibold hover:bg-gray-200"
+                        )}
+                        aria-current={item.name === selectedTab ? "page" : undefined}
+                        onClick={() => handleTabChange(tab.name)}
                       >
-                        <span className="sr-only">Close sidebar</span>
-                        <XMarkIcon
-                          className="h-6 w-6 text-white"
-                          aria-hidden="true"
-                        />
-                      </button>
-                    </div>
-                  </Transition.Child>
-                  {/* Sidebar component, swap this element with another sidebar if you like */}
-                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-50 px-6 pb-4">
-                    <nav className="flex flex-1 flex-col">
-                      <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                        <li>
-                          <ul role="list" className="-mx-2 space-y-1">
-                            {navigation.map((item) => (
-                              <li key={item.name}>
-                                <a
-                                  href={item.to}
-                                  className={cx(
-                                    item.current
-                                      ? "bg-gray-50 text-[#548a6a]"
-                                      : "text-gray-700 hover:text-[#548a6a] hover:bg-gray-50",
-                                    "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                                  )}
-                                >
-                                  <item.icon
-                                    className={cx(
-                                      item.current
-                                        ? "text-[#548a6a]"
-                                        : "text-gray-400 group-hover:text-[#548a6a]",
-                                      "h-6 w-6 shrink-0"
-                                    )}
-                                    aria-hidden="true"
-                                  />
-                                  {item.name}
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
-                        <li className="mt-auto">
-                          <a
-                            href="#"
-                            className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600"
-                          >
-                            <Cog6ToothIcon
-                              className="h-6 w-6 shrink-0 text-gray-400 group-hover:text-[#548a6a]"
-                              aria-hidden="true"
-                            />
-                            Settings
-                          </a>
-                        </li>
-                      </ul>
-                    </nav>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition.Root>
-
-        {/* Static sidebar for desktop */}
-        <div className="mt-[64px] hidden lg:fixed lg:inset-y-0 lg:z-30 lg:flex lg:w-72 lg:flex-col">
-          {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-50 px-6 pb-4">
-            <nav className="flex flex-1 flex-col mt-10">
-              <ul role="list" className="flex flex-1 flex-col gap-y-2">
-                <li className="grow">
-                  <ul role="list" className="-mx-2 space-y-1">
-                    {navigation.map((item, index) => (
-                      <li key={item.name}>
-                        <a
-                          href={item.to}
+                        <item.icon
                           className={cx(
                             item.current
-                              ? "bg-gray-100 text-[#548a6a]"
-                              : "text-gray-700 hover:text-[#548a6a] hover:bg-gray-100",
-                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                              ? "text-[#548a6a]"
+                              : "text-gray-400 group-hover/item:text-[#548a6a]",
+                            "h-6 w-6 shrink-0"
                           )}
-                        >
-                          <item.icon
-                            className={cx(
-                              item.current
-                                ? "text-[#548a6a]"
-                                : "text-gray-400 group-hover:text-[#548a6a]",
-                              "h-6 w-6 shrink-0"
-                            )}
-                            aria-hidden="true"
-                          />
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-                <li className="-mx-6 mt-auto">
+                          aria-hidden="true"
+                        />
+                        <span className="invisible transition-all opacity-0 whitespace-nowrap group-hover/slideout:visible group-hover/slideout:opacity-100">{item.name}</span>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+              <li className="justify-between mt-auto -mx-6">
                   <Link to="settings">
-                    <div className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100">
+                    <div className="flex items-center px-6 py-3 text-sm font-semibold leading-6 text-gray-900 group/settings gap-x-4 hover:bg-gray-200">
                       <Gravatar
                         className="rounded-full"
                         email={user.email}
@@ -211,7 +94,7 @@ const UserPage = () => {
                         size={40}
                       />
                       <span className="sr-only">Your profile</span>
-                      <span aria-hidden="true">
+                      <span aria-hidden="true" className="invisible transition-all opacity-0 whitespace-nowrap group-hover/settings:visible group-hover/settings:opacity-100">
                         {user.first_name && user.last_name
                           ? `${user.first_name} ${user.last_name}`
                           : user.username}
@@ -219,20 +102,18 @@ const UserPage = () => {
                     </div>
                   </Link>
                 </li>
-              </ul>
-            </nav>
-          </div>
+            </ul>
+          </nav>
         </div>
-        <main className="py-10">
-          <div className="px-4 sm:px-6 lg:px-8">
-            <Outlet />
-          </div>
-        </main>
       </div>
+      <main className="grow py-10 ml-[70px]">
+        <div className="px-4 sm:px-6 lg:px-8">
+          <Outlet />
+        </div>
+      </main>
     </div>
   );
 };
-
 // import { Link, Outlet, useLocation } from "react-router-dom";
 // import React, { useEffect, useState } from "react";
 
@@ -273,7 +154,7 @@ const UserPage = () => {
 
 //   return (
 //     <>
-//       <div className="w-full flex items-center py-6 mt-12">
+//       <div className="flex items-center w-full py-6 mt-12">
 //         <div>
 //           <Gravatar
 //             className="rounded-full"
@@ -282,7 +163,7 @@ const UserPage = () => {
 //             size={100}
 //           />
 //         </div>
-//         <div className="grow ml-6">
+//         <div className="ml-6 grow">
 //           <h1 className="text-2xl font-medium text-gray-700 group-hover:text-gray-900">
 //             {user.username}
 //           </h1>
@@ -319,7 +200,7 @@ const UserPage = () => {
 //             </select>
 //           </div>
 //           <div className="hidden sm:block">
-//             <nav className="-mb-px flex space-x-8">
+//             <nav className="flex -mb-px space-x-8">
 //               {tabs.map((tab) => (
 //                 <Link
 //                   key={tab.name}
