15c413704b262b32f79d7ec8305bcf4716eed877
Daniel <lontano.composer@gmail.com>
2023-07-01 22:23:54 -0700

    Add Sentry

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import socket
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -266,7 +268,6 @@ SOCIALACCOUNT_LOGIN_ON_GET = True
 
 LOG_PATH = os.path.join(BASE_DIR, "log")
 
-
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
@@ -282,3 +283,17 @@ LOGGING = {
         "level": "DEBUG",
     },
 }
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    integrations=[
+        DjangoIntegration(),
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
