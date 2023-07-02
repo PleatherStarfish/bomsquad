d6673e702acc965f1a042412de67b5293b2a4108
Daniel <lontano.composer@gmail.com>
2023-07-02 16:25:31 -0700

    No create superuser

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,10 +2,6 @@
 
 python manage.py migrate
 
-if [ "$PRODUCTION" != "True" ]; then
-  python manage.py createsuperuser --noinput
-fi
-
 # Collect static files
 python manage.py collectstatic --noinput --clear --ignore "node_modules" --ignore "rest_framework" --ignore "import_export" --ignore "drf-yasg" --ignore ".DS_Store" --ignore ".DS_Store.*"
 
