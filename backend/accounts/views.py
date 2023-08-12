cfc511b921402f9207d72332cadbdebc521e7008
Daniel <lontano.composer@gmail.com>
2023-08-12 14:12:58 -0700

    Add more logging

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -39,7 +39,6 @@ def delete_user(request):
 
 
 @csrf_exempt
-@api_view(["POST"])
 def kofi_payment_webhook(request):
     verification_token = os.environ.get("KO_FI_VERIFICATION_TOKEN")
     if not verification_token:
@@ -47,29 +46,39 @@ def kofi_payment_webhook(request):
         return HttpResponse(status=400)
 
     data = request.POST.get("data")
+    logger.info("Received webhook request: %s", data)
     if data:
         data_json = json.loads(data)
+        logger.info("Received webhook request: %s", data_json)
         received_verification_token = data_json.get("verification_token")
+        logger.info("Received verification token: %s", received_verification_token)
 
         if received_verification_token != verification_token:
             logger.warning("Invalid verification token received in webhook")
             return HttpResponse(status=400)
 
         email = data_json.get("email")
+        logger.info("Received email: %s", email)
         tier_name = data_json.get("tier_name")
+        logger.info("Received tier name: %s", tier_name)
         kofi_transaction_id_str = data_json.get("kofi_transaction_id")
+        logger.info("Received kofi transaction id: %s", kofi_transaction_id_str)
         timestamp_str = data_json.get("timestamp")
+        logger.info("Received timestamp: %s", timestamp_str)
         is_subscription_payment = bool(data_json.get("is_subscription_payment"))
+        logger.info("Received is subscription payment: %s", is_subscription_payment)
 
         try:
             kofi_transaction_id = (
                 UUID(kofi_transaction_id_str) if kofi_transaction_id_str else None
             )
+            logger.info("Received kofi transaction id: %s", kofi_transaction_id)
         except ValueError:
             return HttpResponse(status=400)
 
         try:
             timestamp = datetime.fromisoformat(timestamp_str) if timestamp_str else None
+            logger.info("Received timestamp: %s", timestamp)
         except ValueError:
             return HttpResponse(status=400)
 
