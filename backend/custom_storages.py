4926a105e3680c6677982ed5403cba6483a0e981
Daniel <lontano.composer@gmail.com>
2024-07-30 16:59:58 -0400

    Fix

--- backend/custom_storages.py
+++ backend/custom_storages.py
@@ -3,13 +3,16 @@ import os
 
 
 class StaticStorage(S3Boto3Storage):
-    bucket_name = os.environ.get("AWS_STORAGE_BUCKET_NAME")
     location = "static"
-    default_acl = "public-read"
+    bucket_name = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+    custom_domain = f"{bucket_name}.digitaloceanspaces.com"
+    region_name = os.environ.get("AWS_S3_REGION_NAME")
+    file_overwrite = False
 
 
 class MediaStorage(S3Boto3Storage):
-    bucket_name = os.environ.get("AWS_STORAGE_BUCKET_NAME")
     location = "media"
-    default_acl = "public-read"
+    bucket_name = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+    custom_domain = f"{bucket_name}.digitaloceanspaces.com"
+    region_name = os.environ.get("AWS_S3_REGION_NAME")
     file_overwrite = False
