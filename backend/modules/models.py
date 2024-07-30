ddaae39f83c2f9b171de31f3fa8b766a1c29f01c
Daniel <lontano.composer@gmail.com>
2024-07-30 11:21:10 -0400

    Fix save method

--- backend/modules/models.py
+++ backend/modules/models.py
@@ -29,7 +29,7 @@ class Manufacturer(BaseModel):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(f"{self.name}")
-            super(Manufacturer, self).save(*args, **kwargs)
+        super(Manufacturer, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name}"
