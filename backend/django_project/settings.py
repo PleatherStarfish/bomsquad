cc1ed2fcf3cdc7ff6e9a13821e1426b8b70d939f
Daniel <lontano.composer@gmail.com>
2024-07-30 17:08:50 -0400

    Fix

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -32,17 +32,6 @@ DEBUG = os.environ.get("DEBUG") == "True"
 ALLOWED_HOSTS = ["*"]
 
 
-class StaticStorage(S3Boto3Storage):
-    location = "static"
-    default_acl = "public-read"
-
-
-class MediaStorage(S3Boto3Storage):
-    location = "media"
-    default_acl = "public-read"
-    file_overwrite = False
-
-
 # Static and media files configuration
 if DEBUG:
     STATIC_URL = "/static/"
