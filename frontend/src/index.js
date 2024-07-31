0006d0b50e4b59307bf15dba847dc53abbb3d3a0
Daniel <lontano.composer@gmail.com>
2024-07-31 16:14:19 -0400

    Fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -33,16 +33,7 @@ const queryClient = new QueryClient({
   },
 });
 
-ReactDOM.createRoot(document.getElementById("root"), {
-  // Callback called when an error is thrown and not caught by an ErrorBoundary.
-  onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
-    console.warn('Uncaught error', error, errorInfo.componentStack);
-  }),
-  // Callback called when React catches an error in an ErrorBoundary.
-  onCaughtError: Sentry.reactErrorHandler(),
-  // Callback called when React automatically recovers from errors.
-  onRecoverableError: Sentry.reactErrorHandler(),
-}).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <React.StrictMode>
       <BrowserRouter>
--- frontend/src/instrument.js
+++ frontend/src/instrument.js
@@ -12,17 +12,18 @@ import { useEffect } from "react";
 Sentry.init({
   dsn: "https://6fe214881fe5713445937c8f65357ee5@o4505458059116544.ingest.us.sentry.io/4507697059135488",
   integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.browserProfilingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        Sentry.reactRouterV6BrowserTracingIntegration({
+          useEffect,
+          useLocation,
+          useNavigationType,
+          createRoutesFromChildren,
+          matchRoutes,
+        }),
+        Sentry.replayIntegration(),
+      ],
+  tracesSampleRate: 1.0,
   tracePropagationTargets: [/^https:\/\/bom-squad\.com\/api/],
-  // Profiling
-  profilesSampleRate: 1.0, // Profile 100% of the transactions. This value is relative to tracesSampleRate
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  profilesSampleRate: 1.0, 
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0,
 });
