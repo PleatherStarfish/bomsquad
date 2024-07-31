1a32df6952b5e7141678af456ffabc84762759f1
Daniel <lontano.composer@gmail.com>
2024-07-31 11:14:47 -0400

    Fix tracePropagationTargets

--- frontend/src/instrument.js
+++ frontend/src/instrument.js
@@ -10,29 +10,19 @@ import {
 import { useEffect } from "react";
 
 Sentry.init({
-  dsn: "https://c47fdba9504144999af70f9c9a098f3c@o4505458059116544.ingest.us.sentry.io/4505458060951552",
+  dsn: "https://6fe214881fe5713445937c8f65357ee5@o4505458059116544.ingest.us.sentry.io/4507697059135488",
   integrations: [
-    // See docs for support of different versions of variation of react router
-    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect,
-      useLocation,
-      useNavigationType,
-      createRoutesFromChildren,
-      matchRoutes,
-    }),
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
     Sentry.replayIntegration(),
   ],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for tracing.
-  tracesSampleRate: 1.0,
-
-  // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
-  tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
-
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [/^https:\/\/bom-squad\.com\/api/],
+  // Profiling
+  profilesSampleRate: 1.0, // Profile 100% of the transactions. This value is relative to tracesSampleRate
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
