8c63d24ab619107b065d7da8c96b7144612e18d6
Daniel <lontano.composer@gmail.com>
2024-10-30 13:32:28 -0700

    Update

--- frontend/src/components/shopping_list/index.js
+++ frontend/src/components/shopping_list/index.js
@@ -186,12 +186,12 @@ const ShoppingList = () => {
         }}
       >
         <div className="flex flex-col gap-4">
-          <span>Do you want to give your saved list a descriptive name?</span>
+          <span>Do you want to attach a descriptive note to your saved list?</span>
           <input 
               type="text" 
               value={notes} 
               onChange={(e) => setNotes(e.target.value)} 
-              placeholder="Name (optional)" 
+              placeholder="Note (optional)" 
               maxLength="1000"
               className="block w-full rounded-md border-0 p-2 h-[32px] text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brandgreen-600 sm:text-sm sm:leading-6"
           />
