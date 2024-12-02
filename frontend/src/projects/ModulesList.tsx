7b764c1dfbe94a777f2df437b3fc214574cda644
Daniel <lontano.composer@gmail.com>
2024-12-02 12:50:44 -0800

    Test

--- frontend/src/projects/ModulesList.tsx
+++ frontend/src/projects/ModulesList.tsx
@@ -65,126 +65,136 @@ const ModulesList: React.FC<ModulesListProps> = ({
   }
 
   return (
-    <motion.div
-      animate={shouldAnimate ? "visible" : "hidden"}
-      className="grid grid-cols-1 gap-y-10 lg:gap-y-14"
-      initial="hidden"
-      variants={containerVariants}
-    >
-      {modules.map((module, index) => (
-        <>
-          <motion.div
-            className="flex flex-col items-center overflow-hidden bg-white rounded-lg md:flex-row"
-            custom={index}
-            key={module.id}
-            variants={slideInFromRight}
-          >
-            <div className="flex justify-center w-full h-64 md:w-48 md:h-48">
-              {module.slug && (
-                <Link reloadDocument to={`/module/${module.slug}`}>
-                  {module.thumb_image_webp && module.thumb_image_jpeg ? (
-                    <picture>
-                      <source
-                        srcSet={module.thumb_image_webp}
-                        type="image/webp"
-                      />
-                      <source
-                        srcSet={module.thumb_image_jpeg}
-                        type="image/jpeg"
-                      />
-                      <img
-                        alt={module.name}
-                        className="object-cover w-full h-full md:object-contain"
-                        src={module.thumb_image_jpeg}
-                      />
-                    </picture>
-                  ) : null}
-                </Link>
-              )}
-            </div>
-            <div className="flex-1 p-4 md:px-4 md:py-5 sm:p-6">
-              <div className="flex flex-wrap justify-between">
-                {module.slug && (
-                  <Link reloadDocument to={`/module/${module.slug}`}>
-                    <h2 className="text-xl font-medium text-gray-900">
-                      {module.name}
-                    </h2>
-                  </Link>
-                )}
-                {user && (
-                  <div className="flex py-2 space-x-2 md:py-0">
-                    <AddModuleButtons moduleId={module.id} queryName="built" />
-                    <AddModuleButtons
-                      moduleId={module.id}
-                      queryName="want-to-build"
-                    />
-                  </div>
-                )}
-              </div>
-              <a
-                className="text-gray-500 hover:text-gray-400"
-                href={`/manufacturer/${module.manufacturer_slug}`}
-              >
-                {module.manufacturer.name}
-              </a>
-              <div className="flex flex-wrap gap-2 mt-2">
-                {module.rack_unit && (
-                  <span className="px-2 py-0.5 text-xs font-semibold text-white rounded-full bg-blue-700">
-                    {module.rack_unit}
-                  </span>
-                )}
-                {module.hp && (
-                  <span
-                    className="px-2 py-0.5 text-xs font-semibold text-white rounded-full"
-                    style={{ backgroundColor: hpColorScale(module.hp).hex() }}
-                  >
-                    {module.hp} HP
-                  </span>
-                )}
-                {module.category && (
-                  <span
-                    className={`px-2 py-0.5 text-xs font-semibold text-white rounded-full ${
-                      module.category.toLowerCase() === "eurorack"
-                        ? "bg-slate-500"
-                        : module.category.toLowerCase() === "pedals"
-                        ? "bg-purple-500"
-                        : module.category.toLowerCase() === "serge"
-                        ? "bg-yellow-800"
-                        : "bg-gray-500"
-                    }`}
-                  >
-                    {module.category}
-                  </span>
-                )}
-              </div>
-              <p className="mt-4 text-sm text-gray-500 line-clamp-5">
-                {module.description}
-              </p>
-            </div>
-          </motion.div>
-
-          {index % 4 === 3 && (
+    <>
+      <Adsense
+        client="ca-pub-1549230942850511"
+        format="rectangle"
+        layout="in-feed"
+        responsive="true"
+        slot="6413550510"
+        style={{ display: "block" }}
+      />
+      <motion.div
+        animate={shouldAnimate ? "visible" : "hidden"}
+        className="grid grid-cols-1 gap-y-10 lg:gap-y-14"
+        initial="hidden"
+        variants={containerVariants}
+      >
+        {modules.map((module, index) => (
+          <>
             <motion.div
-              custom={index + 1}
-              key={`ad-${index}`}
+              className="flex flex-col items-center overflow-hidden bg-white rounded-lg md:flex-row"
+              custom={index}
+              key={module.id}
               variants={slideInFromRight}
             >
-              <ins
-                className="adsbygoogle"
-                data-ad-client="ca-pub-1549230942850511"
-                data-ad-format="rectangle"
-                data-ad-slot="6413550510"
-                data-full-width-responsive="true"
-                style={{display: "block"}}
-              />
-              <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-              </script>
+              <div className="flex justify-center w-full h-64 md:w-48 md:h-48">
+                {module.slug && (
+                  <Link reloadDocument to={`/module/${module.slug}`}>
+                    {module.thumb_image_webp && module.thumb_image_jpeg ? (
+                      <picture>
+                        <source
+                          srcSet={module.thumb_image_webp}
+                          type="image/webp"
+                        />
+                        <source
+                          srcSet={module.thumb_image_jpeg}
+                          type="image/jpeg"
+                        />
+                        <img
+                          alt={module.name}
+                          className="object-cover w-full h-full md:object-contain"
+                          src={module.thumb_image_jpeg}
+                        />
+                      </picture>
+                    ) : null}
+                  </Link>
+                )}
+              </div>
+              <div className="flex-1 p-4 md:px-4 md:py-5 sm:p-6">
+                <div className="flex flex-wrap justify-between">
+                  {module.slug && (
+                    <Link reloadDocument to={`/module/${module.slug}`}>
+                      <h2 className="text-xl font-medium text-gray-900">
+                        {module.name}
+                      </h2>
+                    </Link>
+                  )}
+                  {user && (
+                    <div className="flex py-2 space-x-2 md:py-0">
+                      <AddModuleButtons
+                        moduleId={module.id}
+                        queryName="built"
+                      />
+                      <AddModuleButtons
+                        moduleId={module.id}
+                        queryName="want-to-build"
+                      />
+                    </div>
+                  )}
+                </div>
+                <a
+                  className="text-gray-500 hover:text-gray-400"
+                  href={`/manufacturer/${module.manufacturer_slug}`}
+                >
+                  {module.manufacturer.name}
+                </a>
+                <div className="flex flex-wrap gap-2 mt-2">
+                  {module.rack_unit && (
+                    <span className="px-2 py-0.5 text-xs font-semibold text-white rounded-full bg-blue-700">
+                      {module.rack_unit}
+                    </span>
+                  )}
+                  {module.hp && (
+                    <span
+                      className="px-2 py-0.5 text-xs font-semibold text-white rounded-full"
+                      style={{ backgroundColor: hpColorScale(module.hp).hex() }}
+                    >
+                      {module.hp} HP
+                    </span>
+                  )}
+                  {module.category && (
+                    <span
+                      className={`px-2 py-0.5 text-xs font-semibold text-white rounded-full ${
+                        module.category.toLowerCase() === "eurorack"
+                          ? "bg-slate-500"
+                          : module.category.toLowerCase() === "pedals"
+                          ? "bg-purple-500"
+                          : module.category.toLowerCase() === "serge"
+                          ? "bg-yellow-800"
+                          : "bg-gray-500"
+                      }`}
+                    >
+                      {module.category}
+                    </span>
+                  )}
+                </div>
+                <p className="mt-4 text-sm text-gray-500 line-clamp-5">
+                  {module.description}
+                </p>
+              </div>
             </motion.div>
-          )}
-        </>
-      ))}
-    </motion.div>
+
+            {index % 4 === 3 && (
+              <motion.div
+                custom={index + 1}
+                key={`ad-${index}`}
+                variants={slideInFromRight}
+              >
+                <Adsense
+                  client="ca-pub-1549230942850511"
+                  format="rectangle"
+                  layout="in-feed"
+                  responsive="true"
+                  slot="6413550510"
+                  style={{ display: "block" }}
+                />
+              </motion.div>
+            )}
+          </>
+        ))}
+      </motion.div>
+    </>
   );
 };
 
