43d93a976116876f8f658bbf4fb9e045d0e85cd8
Daniel <lontano.composer@gmail.com>
2024-12-21 16:37:23 -0800

    Update modal

--- backend/static/js/banner.js
+++ backend/static/js/banner.js
@@ -1,32 +1,37 @@
 window.addEventListener("load", function () {
-  var banner = cash("#banner");
+  var bannerModal = cash("#banner-modal");
   var hideButton = cash("#banner-hide-button");
   var hiddenTime = localStorage.getItem("bannerHiddenTime");
   var hideDuration = localStorage.getItem("bannerHideDuration");
 
-  // Check if banner should be displayed immediately
-  if (
-    !hiddenTime ||
-    !hideDuration ||
-    Date.now() - parseInt(hiddenTime) >= parseInt(hideDuration)
-  ) {
-    // No hidden time or duration set, or the duration has passed; show the banner
-    banner.removeClass("hidden");
-  }
+  var randomDelay = Math.floor(Math.random() * (60000 - 6000 + 1)) + 6000;
 
-  // Add a click event listener to the hide button
-  hideButton.on("click", function () {
-    // Hide the banner
-    banner.addClass("hidden");
+  // Delay the execution of modal logic by 6 seconds
+  setTimeout(function () {
+    // Check if the modal should be displayed
+    if (
+      !hiddenTime ||
+      !hideDuration ||
+      Date.now() - parseInt(hiddenTime) >= parseInt(hideDuration)
+    ) {
+      // No hidden time or duration set, or the duration has passed; show the modal
+      bannerModal.removeClass("hidden");
+    }
 
-    // Generate a random number of days between 1 and 7
-    var randomDays = Math.floor(Math.random() * 4) + 1;
+    // Add a click event listener to the hide button
+    hideButton.on("click", function () {
+      // Hide the modal
+      bannerModal.addClass("hidden");
 
-    // Convert days to milliseconds for the duration
-    var randomDuration = randomDays * 24 * 60 * 60 * 1000;
+      // Generate a random number of days between 1 and 3
+      var randomDays = Math.floor(Math.random() * 3) + 1;
 
-    // Store the current time and random duration in local storage
-    localStorage.setItem("bannerHiddenTime", Date.now());
-    localStorage.setItem("bannerHideDuration", randomDuration);
-  });
+      // Convert days to milliseconds for the duration
+      var randomDuration = randomDays * 24 * 60 * 60 * 1000;
+
+      // Store the current time and random duration in local storage
+      localStorage.setItem("bannerHiddenTime", Date.now());
+      localStorage.setItem("bannerHideDuration", randomDuration);
+    });
+  }, randomDelay);
 });
